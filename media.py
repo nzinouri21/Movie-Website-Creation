db5e346929f648d64b342592a164764895d9fd24
Nazanin <n.zinouri@gmail.com>
2017-12-22 11:12:43 -0500

    adding another file

---
+++ media.py
@@ -0,0 +1,9 @@
+#Things we want the class movie to remember:
+#   1.Title
+#   2.Storyline
+#   3.Poster image
+#   4.YouTube trailer
+
+#Based on "Google Python Style Guide", when defining a class name,
+#the first letter should be upper-case
+class Movie():
